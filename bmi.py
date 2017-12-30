72e10b7e0e3901b930f351008ac28efae0a663eb
zachsanford <sanfordzachary@gmail.com>
2017-12-30 15:20:37 -0800

    Changes
    Just some minor changes.

--- bmi.py
+++ bmi.py
@@ -3,35 +3,61 @@
 # Author: Zachary Sanford (@zachsanford)
 # Published Dec 20th, 2017
 
-print("**************** BMI Calculator ****************\n")
-print("Hello, let's figure out your Body Mass Index!\n")
-lbs = int(input("Please enter your weight in pounds (lbs) >> "))
-inches = int(input("\nPlease enter your height in inches >> "))
+import os
+os.system('clear')
+CONTINUE = True
 
-def poundsToKilos(var):
-	kilos = var * 0.45359237
-	return kilos
+# For user to continue I used a while loop
+# as well as a boolean variable.
 
-kg = poundsToKilos(lbs)
+while CONTINUE:
+	print("**************** BMI Calculator ****************\n")
+	print("Hello, let's figure out your Body Mass Index!\n")
+	lbs = int(input("Please enter your weight in pounds (lbs) >> "))
+	inches = int(input("\nPlease enter your height in inches >> "))
 
-def inchesToMeters(x):
-	meters = x * 0.0254
-	return meters
+	def poundsToKilos(var):
+		kilos = var * 0.45359237
+		return kilos
 
-m = inchesToMeters(inches)
+	kg = poundsToKilos(lbs)
 
-def bodyMassIndex(weight,height):
-	y = weight / (height ** 2)
-	return y
+	def inchesToMeters(x):
+		meters = x * 0.0254
+		return meters
 
-bmi = bodyMassIndex(kg,m)
+	m = inchesToMeters(inches)
 
-print("\nYour BMI is",bmi)
-if bmi >= 30:
-	print("\nYour BMI Category is OBESITY.")
-elif bmi >= 25:
-	print("\nYour BMI Category is OVERWEIGHT.")
-elif bmi >= 18.5:
-	print("\nYour BMI Category is NORMAL WEIGHT.")
-else:
-	print("\nYour BMI Category is UNDERWEIGHT.")
+	def bodyMassIndex(weight,height):
+		y = weight / (height ** 2)
+		return y
+
+	bmi = bodyMassIndex(kg,m)
+
+	print("\nYour BMI is",bmi)
+	if bmi >= 30:
+		print("\nYour BMI Category is OBESITY.")
+	elif bmi >= 25:
+		print("\nYour BMI Category is OVERWEIGHT.")
+	elif bmi >= 18.5:
+		print("\nYour BMI Category is NORMAL WEIGHT.")
+	else:
+		print("\nYour BMI Category is UNDERWEIGHT.")
+	
+	userAnswer = input("\nWould you like to try again? (Y/N) >> ")
+	if userAnswer == "Y":
+		CONTINUE = True
+		os.system('clear')
+	elif userAnswer == "y":
+		CONTINUE = True
+		os.system('clear')
+	elif userAnswer == "N":
+		CONTINUE = False
+		os.system('clear')
+		input("Goodbye!\n\nPress any key to quit...")
+		os.system('clear')
+	elif userAnswer == "n":
+		CONTINUE = False
+		os.system('clear')
+		input("Goodbye!\n\nPress any key to quit...")
+		os.system('clear')
