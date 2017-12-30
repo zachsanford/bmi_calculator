b9a83318c463936767fbd0bb014499efb9de8e13
zachsanford <sanfordzachary@gmail.com>
2017-12-30 14:26:30 -0800

    Add files via upload

---
+++ bmi.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+print("**************** BMI Calculator ****************\n")
+print("Hello, let's figure out your Body Mass Index!\n")
+lbs = int(input("Please enter your weight in pounds (lbs) >> "))
+inches = int(input("\nPlease enter your height in inches >> "))
+
+def poundsToKilos(var):
+	kilos = var * 0.45359237
+	return kilos
+
+kg = poundsToKilos(lbs)
+
+def inchesToMeters(x):
+	meters = x * 0.0254
+	return meters
+
+m = inchesToMeters(inches)
+
+def bodyMassIndex(weight,height):
+	y = weight / (height ** 2)
+	return y
+
+bmi = bodyMassIndex(kg,m)
+
+print("\nYour BMI is",bmi)
+if bmi >= 30:
+	print("\nYour BMI Category is OBESITY.")
+elif bmi >= 25:
+	print("\nYour BMI Category is OVERWEIGHT.")
+elif bmi >= 18.5:
+	print("\nYour BMI Category is NORMAL WEIGHT.")
+else:
+	print("\nYour BMI Category is UNDERWEIGHT.")
